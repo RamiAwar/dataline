7c3cdc20adc294b78588f40f9ed7b44332b60451
Rami <rami.awar.ra@gmail.com>
2024-03-03 14:16:19 +0100

    feat: Improve storybook support

--- text2sql-frontend/.storybook/preview.ts
+++ text2sql-frontend/.storybook/preview.ts
@@ -2,7 +2,15 @@ import type { Preview } from "@storybook/react";
 import "../src/index.css";
 
 const preview: Preview = {
+  globalTypes: {
+    darkMode: {
+      defaultValue: true, // Enable dark mode by default on all stories
+    },
+  },
   parameters: {
+    backgrounds: {
+      default: "dark",
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
