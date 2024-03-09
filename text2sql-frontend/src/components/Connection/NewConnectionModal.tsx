ac32606aa52ac172046e894c776c8c1a225b5307
Anthony Malkoun <antounmalkoun@gmail.com>
2024-03-10 01:56:45 +0200

    Add helper text/command to easily setup test db

--- text2sql-frontend/src/components/Connection/NewConnectionModal.tsx
+++ text2sql-frontend/src/components/Connection/NewConnectionModal.tsx
@@ -88,7 +88,17 @@ function NewConnectionModal({ isOpen, onClose }: NewConnectionModalFormProps) {
                       New Connection
                     </h2>
                     <p className="mt-1 text-sm leading-6 text-gray-400">
-                      Add a new database connection
+                      Add a new database connection.{" "}
+                      <a
+                        className=" underline cursor-pointer"
+                        onClick={() =>
+                          alert(
+                            "Run this command:\n```\ndocker run -p 5433:5432 --name dvdrental -d anthony2261/dvdrental_db:latest\n```\nand use this connection string:\npostgresql://postgres:dvdrental@localhost:5433/dvdrental"
+                          )
+                        }
+                      >
+                        Need to setup a quick test db?
+                      </a>
                     </p>
 
                     <div className="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -128,9 +138,9 @@ function NewConnectionModal({ isOpen, onClose }: NewConnectionModalFormProps) {
                         Connection string / DSN
                       </label>
                       {/* Hidden input field overlaying the real one
-                      
+
                       <div className="relative mt-2">
-                        
+
                         <input
                           type="text"
                           autoComplete="off"
