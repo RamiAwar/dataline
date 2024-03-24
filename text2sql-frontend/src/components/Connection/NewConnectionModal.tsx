d1dea50120651f5a14406e832ddaf9da5d482f4d
Anthony Malkoun <antounmalkoun@gmail.com>
2024-03-24 18:49:41 +0200

    Fixes #77

--- text2sql-frontend/src/components/Connection/NewConnectionModal.tsx
+++ text2sql-frontend/src/components/Connection/NewConnectionModal.tsx
@@ -211,8 +211,13 @@ function NewConnectionModal({ isOpen, onClose }: NewConnectionModalFormProps) {
                       Cancel
                     </button>
                     <button
+                      disabled={!connectionName || !unmaskedDsn}
                       type="submit"
-                      className="inline-flex items-center rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                      className={classNames(
+                        "inline-flex items-center rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold text-white shadow-sm",
+                        "hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500",
+                        "disabled:bg-gray-600 disabled:hover:bg-gray-600 disabled:text-gray-300"
+                      )}
                     >
                       {isLoading && (
                         <Spinner className="pointer-events-none h-5 w-5"></Spinner>
