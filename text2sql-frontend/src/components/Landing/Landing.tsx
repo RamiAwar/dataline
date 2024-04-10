071c5238a9935fc8ac03f7756c59334bae08bb43
Rami <rami.awar.ra@gmail.com>
2024-04-10 18:20:19 +0200

    docs: Added alt

--- text2sql-frontend/src/components/Landing/Landing.tsx
+++ text2sql-frontend/src/components/Landing/Landing.tsx
@@ -238,7 +238,7 @@ export const Landing = () => {
               </div>
               <img
                 src={connection_creator_image}
-                alt="Product screenshot"
+                alt="Connection creator screenshot"
                 className="w-[40rem] max-w-none rounded-xl shadow-xl ring-1 ring-white/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
                 width={2432}
                 height={1442}
