1ed3880594975cbae8c678f34226d1baacb0b49e
Rami <rami.awar.ra@gmail.com>
2024-05-10 12:34:06 +0200

    refactor: Remove llama index dependency

--- text2sql-backend/sql_wrapper.py
+++ text2sql-backend/sql_wrapper.py
@@ -3,11 +3,10 @@
 import logging
 from collections import defaultdict
 from contextvars import ContextVar
-from typing import Any, Iterable, Literal
+from typing import Any, Iterable, List, Literal, Optional
 
-from llama_index import SQLDatabase
 from rapidfuzz import fuzz
-from sqlalchemy import text
+from sqlalchemy import Engine, MetaData, inspect, text
 from sqlalchemy.exc import ProgrammingError
 
 from dataline.models.connection.schema import TableSchema
@@ -18,14 +17,96 @@ request_limit = ContextVar[int]("request_limit")
 request_execute = ContextVar[bool]("request_execute")
 
 
-class CustomSQLDatabase(SQLDatabase):
+class CustomSQLDatabase:
+    def __init__(
+        self,
+        engine: Engine,
+        schema: Optional[str] = None,
+        metadata: Optional[MetaData] = None,
+        ignore_tables: Optional[List[str]] = None,
+        include_tables: Optional[List[str]] = None,
+        sample_rows_in_table_info: int = 3,
+        indexes_in_table_info: bool = False,
+        custom_table_info: Optional[dict] = None,
+        view_support: bool = False,
+        max_string_length: int = 300,
+        lazy_table_reflection: bool = False,
+    ):
+        """Create engine from database URI."""
+        self._engine = engine
+        self._schema = schema
+        if include_tables and ignore_tables:
+            raise ValueError("Cannot specify both include_tables and ignore_tables")
+
+        self._inspector = inspect(self._engine)
+
+        # including view support by adding the views as well as tables to the all
+        # tables list if view_support is True
+        self._all_tables = set(
+            self._inspector.get_table_names(schema=schema)
+            + (self._inspector.get_view_names(schema=schema) if view_support else [])
+        )
+
+        self._include_tables = set(include_tables) if include_tables else set()
+        if self._include_tables:
+            missing_tables = self._include_tables - self._all_tables
+            if missing_tables:
+                raise ValueError(f"include_tables {missing_tables} not found in database")
+        self._ignore_tables = set(ignore_tables) if ignore_tables else set()
+        if self._ignore_tables:
+            missing_tables = self._ignore_tables - self._all_tables
+            if missing_tables:
+                raise ValueError(f"ignore_tables {missing_tables} not found in database")
+        usable_tables = self.get_usable_table_names()
+        self._usable_tables = set(usable_tables) if usable_tables else self._all_tables
+
+        if not isinstance(sample_rows_in_table_info, int):
+            raise TypeError("sample_rows_in_table_info must be an integer")
+
+        self._sample_rows_in_table_info = sample_rows_in_table_info
+        self._indexes_in_table_info = indexes_in_table_info
+
+        self._custom_table_info = custom_table_info
+        if self._custom_table_info:
+            if not isinstance(self._custom_table_info, dict):
+                raise TypeError(
+                    "table_info must be a dictionary with table names as keys and the " "desired table info as values"
+                )
+            # only keep the tables that are also present in the database
+            intersection = set(self._custom_table_info).intersection(self._all_tables)
+            self._custom_table_info = dict(
+                (table, self._custom_table_info[table]) for table in self._custom_table_info if table in intersection
+            )
+
+        self._max_string_length = max_string_length
+        self._view_support = view_support
+
+        self._metadata = metadata or MetaData()
+        if not lazy_table_reflection:
+            # including view support if view_support = true
+            self._metadata.reflect(
+                views=view_support,
+                bind=self._engine,
+                only=list(self._usable_tables),
+                schema=self._schema,
+            )
+
     """Custom SQL Database wrapper"""
+
+    def get_usable_table_names(self) -> Iterable[str]:
+        """Get names of tables available."""
+        if self._include_tables:
+            return sorted(self._include_tables)
+        return sorted(self._all_tables - self._ignore_tables)
 
     def get_table_names(self) -> Iterable[str]:
         """Get names of tables available."""
         return self.get_usable_table_names()
 
-    def get_closest_table_name(self, table_name) -> Any | str | None:
+    def get_table_columns(self, table_name: str) -> list[Any]:  # type: ignore
+        return self._inspector.get_columns(table_name)
+
+    def get_closest_table_name(self, table_name: str) -> Any | str | None:  # type: ignore[misc]
         """Get the closest table name to the given table name."""
         table_names = self.get_usable_table_names()
         if table_name in table_names:
