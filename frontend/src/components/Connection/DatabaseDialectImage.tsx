1c9ba83a449147adc77c88abc1722764eb6897bf
Anthony Malkoun <antounmalkoun@gmail.com>
2024-06-30 13:47:11 +0300

    Add mysql cute logo. It's really cute.

--- frontend/src/components/Connection/DatabaseDialectImage.tsx
+++ frontend/src/components/Connection/DatabaseDialectImage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import postgresImage from "../../assets/images/postgres.png";
 import sqliteImage from "../../assets/images/sqlite.png";
 import snowflakeImage from "../../assets/images/snowflake.png";
+import mysqlImage from "../../assets/images/mysql.png";
 import { CircleStackIcon } from "@heroicons/react/24/outline";
 
 interface DatabaseDialectImageProps {
@@ -15,13 +16,14 @@ const DatabaseDialectImage: React.FC<DatabaseDialectImageProps> = ({
     postgresql: postgresImage,
     sqlite: sqliteImage,
     snowflake: snowflakeImage,
+    mysql: mysqlImage,
     // You can add more mappings here as needed
   };
 
   const imageSrc = imageMap[databaseDialect];
 
   if (!imageSrc) {
-    return <CircleStackIcon className="text-gray-400" />;
+    return <CircleStackIcon className="text-gray-400 h-full" />;
   }
 
   return <img className="h-full" src={imageSrc} alt={databaseDialect} />;
