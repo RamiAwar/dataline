361510f1263d6d390a59f540dab6b460c0bde202
Rami <rami.awar.ra@gmail.com>
2024-07-04 09:39:07 +0200

    feat: Improve landing page

--- frontend/src/components/Landing/InstallSection.tsx
+++ frontend/src/components/Landing/InstallSection.tsx
@@ -30,7 +30,7 @@ export const InstallSection = () => {
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="mx-auto flex max-w-4xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-20 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
           <img
-            className="hidden lg:block h-32 w-full flex-none rounded-2xl object-contain lg:aspect-square lg:h-auto lg:max-w-[20vw]"
+            className="hidden lg:block h-32 w-full flex-none rounded-2xl object-contain lg:aspect-square lg:h-auto lg:max-w-[15vw] 2xl:max-w-[10vw]"
             src={logo_w_border}
             alt=""
           />
@@ -74,18 +74,6 @@ export const InstallSection = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div
-        className="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
-        aria-hidden="true"
-      >
-        <div
-          className="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
-          style={{
-            clipPath:
-              "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
-          }}
-        />
       </div>
     </div>
   );
