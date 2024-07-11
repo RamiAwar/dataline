3ed2a52ef6460168e2b5b6922d7f1b35664b373b
Anthony Malkoun <antounmalkoun@gmail.com>
2024-07-11 19:12:01 +0300

    Fix CORS issue when no auth required

--- frontend/src/services/api_client.ts
+++ frontend/src/services/api_client.ts
@@ -1,12 +1,16 @@
 import axios, { AxiosRequestConfig, AxiosResponse, Method } from "axios";
 
 // Initializing Axios instance
-const axiosInstance = axios.create({ withCredentials: true });
+const axiosInstance = axios.create();
 
 export const apiURL = import.meta.env.VITE_API_URL || "http://localhost:7377";
 
 interface AxiosRequestConfigPatch extends Omit<AxiosRequestConfig, "method"> {
   method?: Method; // axios sets it as string, which is unhelpful and can lead to bugs
+}
+
+export function configureAxiosInstance(withCredentials: boolean) {
+  axiosInstance.defaults.withCredentials = withCredentials;
 }
 
 export function backendApi<T>(
